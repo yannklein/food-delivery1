3c687846c6515a633b346abc0fb84d3db27a1af1
Yann Klein <yann.klein@me.com>
2022-05-13 17:27:03 +0900

    app.rb inital stage

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require_relative "router"
 
-
 MEALS_CSV_FILE = File.join(__dir__, "data/meals.csv")
 CUSTOMERS_CSV_FILE = File.join(__dir__, "data/customers.csv")
 
-router = Router.new
 router.run
